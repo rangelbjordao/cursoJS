2655440535388f6f76b958bc9b6fe2ef5f60e3b9
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2023-07-06 17:35:00 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -5,6 +5,13 @@ console.log(num);
 console.log(`O vetor tem ${num.length} posicoes`)
 console.log(`O primeiro valor do vetor e ${num[0]}`)
 
+let pos = num.indexOf(4)
+if (pos == -1) {
+    console.log('O valor nao foi encontrado');
+} else {
+    console.log(`O valor 8 esta na posicao ${pos}`);
+}
+
 for (let pos = 0; pos < num.length; pos++) {
     console.log(num[pos])
 }
