7797996195c9207301661ad8caec77518c9705bd
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2023-07-12 16:02:52 -0300

    exercicio 16 atualizado

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,13 +4,27 @@ function contar() {
     var fim = document.querySelector('input#txtfim')
     var passo = document.querySelector('input#txtpas')
 
-    inicio = Number(inicio.value)
-    fim = Number(fim.value)
-    passo = Number(passo.value)
-    
-    res.innerHTML = `<br>Contando: <br> `
-    while (inicio <= fim) {
-        res.innerHTML += `${inicio} `
-        inicio += passo
+
+    if (inicio.value.length == 0) {
+        res.innerHTML = `<br>Impossivel contar`
+    } else if (Number(passo.value) == 0) {
+        alert(`Passo invalido! Considerando PASSO 1`)
+        passo = 1
+        inicio = Number(inicio.value)
+        fim = Number(fim.value)
+        res.innerHTML = `<br>Contando: <br> `
+        while (inicio <= fim) {
+            res.innerHTML += `${inicio} `
+            inicio += passo
+        }
+    } else {
+        inicio = Number(inicio.value)
+        fim = Number(fim.value)
+        passo = Number(passo.value)
+        res.innerHTML = `<br>Contando: <br> `
+        while (inicio <= fim) {
+            res.innerHTML += `${inicio} `
+            inicio += passo
+        }
     }
 }
