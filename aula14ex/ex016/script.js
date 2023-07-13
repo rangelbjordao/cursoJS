a16ebbb5d0dd594745a6d9b70b85331ad43c18b8
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2023-07-13 15:02:34 -0300

    exercico 16 atualizado

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,29 +1,33 @@
 function contar() {
-    var res = document.querySelector('div#res')
-    var inicio = document.querySelector('input#txtini')
-    var fim = document.querySelector('input#txtfim')
-    var passo = document.querySelector('input#txtpas')
-    
-    if (inicio.value.length == 0) {
-        res.innerHTML = `<br>Impossivel contar`
-    } else if (Number(passo.value) == 0) {
-        alert(`Passo invalido! Considerando PASSO 1`)
-        passo = 1
-        inicio = Number(inicio.value)
-        fim = Number(fim.value)
-        res.innerHTML = `<br>Contando: <br> `
-        while (inicio <= fim) {
-            res.innerHTML += `${inicio} `
-            inicio += passo
-        }
+    let inicio = document.querySelector('input#txtini')
+    let fim = document.querySelector('input#txtfim')
+    let passo = document.querySelector('input#txtpas')
+    let res = document.querySelector('div#res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = `Impossível contar`
     } else {
-        inicio = Number(inicio.value)
-        fim = Number(fim.value)
-        passo = Number(passo.value)
-        res.innerHTML = `<br>Contando: <br> `
-        while (inicio <= fim) {
-            res.innerHTML += `${inicio} `
-            inicio += passo
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0) {
+            window.alert('Passo invalido! Considerando PASSO 1')
+            p = 1
         }
+
+        if (i < f) {
+            // crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            // regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
